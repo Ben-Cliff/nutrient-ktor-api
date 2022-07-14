21dea8b03b1ba038af94a64582dd8da1ea26ca76
ben <benjamin.trollope@deliveryhero.com>
2022-07-14 13:27:35 +0200

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.usda-nutrient-ktor-api"
---
+++ src/main/kotlin/com/usda-nutrient/Application.kt
@@ -0,0 +1,13 @@
+package com.usda-nutrient
+
+import io.ktor.server.application.*
+import com.usda-nutrient.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
