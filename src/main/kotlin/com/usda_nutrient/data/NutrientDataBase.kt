175fd7c0fd3b5a19af7e8074c2472b084faddfe9
ben <benjamin.trollope@deliveryhero.com>
2022-07-14 15:09:17 +0200

    init mongodb client and db

---
+++ src/main/kotlin/com/usda_nutrient/data/NutrientDataBase.kt
@@ -0,0 +1,7 @@
+package com.usda_nutrient.data
+
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+private val client = KMongo.createClient().coroutine
+private val database = client.getDatabase("NutritionalDatabase")
