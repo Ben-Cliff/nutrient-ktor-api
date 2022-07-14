bfc6b892b364068291b5bf8c7ee881b218097a73
ben <benjamin.trollope@deliveryhero.com>
2022-07-14 15:44:44 +0200

    create variable to access collection where items are kepts

--- src/main/kotlin/com/usda_nutrient/data/NutrientDataBase.kt
+++ src/main/kotlin/com/usda_nutrient/data/NutrientDataBase.kt
@@ -1,7 +1,10 @@
 package com.usda_nutrient.data
 
+import com.usda_nutrient.data.model.Item
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
 private val client = KMongo.createClient().coroutine
 private val database = client.getDatabase("NutritionalDatabase")
+
+private val items = database.getCollection<Item>()
