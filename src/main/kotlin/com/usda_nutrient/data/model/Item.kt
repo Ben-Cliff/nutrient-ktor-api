ffa3f1ef7a94f4f1fb41d03280996a589a032e76
ben <benjamin.trollope@deliveryhero.com>
2022-07-14 15:43:53 +0200

    specifying data structure for item and associated nutrion for each item

--- src/main/kotlin/com/usda_nutrient/data/model/Item.kt
+++ src/main/kotlin/com/usda_nutrient/data/model/Item.kt
@@ -1,4 +1,14 @@
 package com.usda_nutrient.data.model
 
-class Item {
-}
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+data class Item (
+    val FoodGroup: String,
+    val Description: String,
+    val Energy_kcal: Int,
+    val Protein_g: Int,
+    val Fat_g: Int,
+    @BsonId
+    val ID: String = ObjectId().toString()
+)
